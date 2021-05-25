1e7cdd24b6aab4e63d7e7ac31ae1f21e59547c3d
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-05-25 13:01:03 +0530

    [Priyanshu] added check connection

---
+++ AddressBookADO/DBConnection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookADO
+{
+    public class DBConnection
+    {
+        
+        public SqlConnection GetConnection()
+        {
+            /// Specifying the connectionString from the sql server connection.
+            string connectiongString = @"Data Source=LAPTOP-NAVJ6800\SQLEXPRESS;Initial Catalog=AddressBook;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectiongString);
+            return connection;
+        }
+    }
+}
